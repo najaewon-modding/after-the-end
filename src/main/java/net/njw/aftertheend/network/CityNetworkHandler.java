6e1dd8de093f724512e94dcaeb212d44cfab1ec8
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-13 00:30:39 +0900

    feat: enforce altar-gated city travel

--- src/main/java/net/njw/aftertheend/network/CityNetworkHandler.java
+++ src/main/java/net/njw/aftertheend/network/CityNetworkHandler.java
@@ -2,7 +2,6 @@ package net.njw.aftertheend.network;
 
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
-import net.njw.aftertheend.city.CityTeleportService;
 
 public final class CityNetworkHandler {
     private static final String NETWORK_VERSION = "3";
@@ -13,6 +12,6 @@ public final class CityNetworkHandler {
         PayloadRegistrar registrar = event.registrar(NETWORK_VERSION);
         registrar.playToClient(CitySyncPayload.TYPE, CitySyncPayload.STREAM_CODEC);
         registrar.playToClient(AltarActivationPayload.TYPE, AltarActivationPayload.STREAM_CODEC);
-        registrar.playToServer(CityTeleportRequestPayload.TYPE, CityTeleportRequestPayload.STREAM_CODEC, CityTeleportService::handleRequest);
+        registrar.playToServer(CityTeleportRequestPayload.TYPE, CityTeleportRequestPayload.STREAM_CODEC, CityTeleportGate::handleRequest);
     }
 }
