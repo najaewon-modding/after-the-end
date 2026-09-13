fa4cb697c64a7122f09a875761cfe58b70f26e75
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-13 13:14:45 +0900

    register city recall sound payload

--- src/main/java/net/njw/aftertheend/network/CityNetworkHandler.java
+++ src/main/java/net/njw/aftertheend/network/CityNetworkHandler.java
@@ -5,7 +5,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.njw.aftertheend.city.CityTeleportService;
 
 public final class CityNetworkHandler {
-    private static final String NETWORK_VERSION = "5";
+    private static final String NETWORK_VERSION = "6";
 
     private CityNetworkHandler() { }
 
@@ -13,6 +13,7 @@ public final class CityNetworkHandler {
         PayloadRegistrar registrar = event.registrar(NETWORK_VERSION);
         registrar.playToClient(CitySyncPayload.TYPE, CitySyncPayload.STREAM_CODEC);
         registrar.playToClient(AltarActivationPayload.TYPE, AltarActivationPayload.STREAM_CODEC);
+        registrar.playToClient(CityRecallSoundPayload.TYPE, CityRecallSoundPayload.STREAM_CODEC);
         registrar.playToServer(CityTeleportRequestPayload.TYPE, CityTeleportRequestPayload.STREAM_CODEC, CityTeleportGate::handleRequest);
         registrar.playToServer(CityArrivalAltarRequestPayload.TYPE, CityArrivalAltarRequestPayload.STREAM_CODEC, CityTeleportService::handleArrivalAltarRequest);
         registrar.playToServer(CityRenameRequestPayload.TYPE, CityRenameRequestPayload.STREAM_CODEC, CityRenameRequestPayload::handle);
